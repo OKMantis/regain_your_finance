dd3921744f7cb993e953168fe0ae7f8ad8e0a9fe
Owen Moss <33687855+OKMantis@users.noreply.github.com>
2026-05-30 14:54:41 +0200

    Add progress_bar_color and progress_bar_width helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -13,4 +13,22 @@ module ApplicationHelper
     cents = yearly ? item.amount_cents_monthly * 12 : item.amount_cents_monthly
     format_euros(cents)
   end
+
+  def progress_bar_color(spent_cents, target_cents)
+    return nil if target_cents.nil? || target_cents.zero?
+    pct = spent_cents * 100.0 / target_cents
+    if pct < 80
+      "bg-emerald-500"
+    elsif pct < 100
+      "bg-amber-500"
+    else
+      "bg-red-500"
+    end
+  end
+
+  def progress_bar_width(spent_cents, target_cents)
+    return "0%" if target_cents.nil? || target_cents.zero?
+    pct = [spent_cents * 100.0 / target_cents, 100].min
+    "#{pct.round(1)}%"
+  end
 end
